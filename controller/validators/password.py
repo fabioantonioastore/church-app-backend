f7a5be346b4027ac0646358aea88c7baa6b181f1
ninja <astore.a.fabio@gmail.com>
2024-07-20 15:05:07 -0300

    login

--- controller/validators/password.py
+++ controller/validators/password.py
@@ -4,9 +4,16 @@ from controller.errors.password_error import PasswordError
 class PasswordValidator():
     def __init__(self, password: str):
         self.password = password
+        self.has_minimium_characters()
+        self.has_space()
         self.has_upper_letter()
         self.has_number()
         self.has_special_character()
+
+    def has_minimium_characters(self):
+        if len(self.password) < 8: raise PasswordError("Password have to have a minimium of 8 characteres")
+    def has_space(self):
+        if " " in self.password: raise PasswordError("Password cannot have space")
 
     def has_upper_letter(self) -> None:
         if not(re.search(r'[A-Z]', self.password)): raise PasswordError("Password don't have upper case letter")

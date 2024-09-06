5a7532adca3f967517c0b3bf67e26700e9b22330
ninja <astore.a.fabio@gmail.com>
2024-09-06 01:14:23 -0300

    firebase

--- controller/auth/firebase.py
+++ controller/auth/firebase.py
@@ -1,10 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
-from typing import NoReturn
 
 FIREBASE_CREDENTIALS_PATH = "firebase_credentials.json"
 
 
-def initialize_firebase() -> NoReturn:
+def initialize_firebase():
     cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
     firebase_admin.initialize_app(cred)

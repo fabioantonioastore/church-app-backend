fa4cbeb52a2669e43e171b8d7ff19f6e7da9647b
ninja <astore.a.fabio@gmail.com>
2024-10-06 12:29:54 -0300

    notification for warning

--- controller/auth/jwt.py
+++ controller/auth/jwt.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 SECRET_KEY = getenv('SECRET_KEY')
 ALGORITHM = getenv('ALGORITHM')
-EXPIRE_MINUTES = 60 * 24
+EXPIRE_MINUTES = 60 * 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/signin")
 
 def create_access_token(cpf: str, position: str = "user"):

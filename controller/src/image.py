ba2014b9143d4d81f2e786c8d2ff6817de1bcb65
ninja <astore.a.fabio@gmail.com>
2024-10-29 09:47:42 -0300

    refactor

--- controller/src/image.py
+++ controller/src/image.py
@@ -1,7 +1,7 @@
 from fastapi import File, UploadFile
 
 
-def verify_if_file_is_png_or_jpeg(file: UploadFile) -> bool:
+def is_png_or_jpeg_image(file: UploadFile) -> bool:
     return file.content_type == "image/jpeg" or file.content_type == "image/png"
 
 
--- router/image.py
+++ router/image.py
@@ -1,7 +1,7 @@
 from fastapi import File, UploadFile, APIRouter, Depends, status
 from router.middleware.authorization import verify_user_access_token
 import os
-from controller.src.image import is_png_image, is_jpeg_image
+from controller.src.image import is_png_or_jpeg_image
 from controller.errors.http.exceptions import not_acceptable
 from controller.crud.user import UserCrud
 
@@ -13,7 +13,7 @@ router = APIRouter()
 @router.put("/upload/image/user", status_code=status.HTTP_204_NO_CONTENT,
             dependencies=[Depends(verify_user_access_token)])
 async def upload_image(user: Depends(verify_user_access_token), file: UploadFile = File(...)):
-    if is_png_image(file) or is_jpeg_image(file):
+    if is_png_or_jpeg_image(file):
         image_data = await file.read()
         user = await user_crud.update_user_image(user['cpf'], image_data)
         return

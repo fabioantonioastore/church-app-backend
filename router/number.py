222bf20b2a915f06ccb055986a6fb5f4d95a5fa3
ninja <astore.a.fabio@gmail.com>
2024-11-14 18:39:29 -0300

    sms

--- router/number.py
+++ router/number.py
@@ -9,18 +9,21 @@ login_crud = LoginCrud()
 user_crud = UserCrud()
 
 
-@router.get('/password_recovery/{number}', status_code=status.HTTP_204_NO_CONTENT)
-async def get_password_recovery_code(number: str):
+@router.get('/password_recovery/{cpf}', status_code=status.HTTP_200_OK)
+async def get_password_recovery_code(cpf: str):
+    user = await user_crud.get_user_by_cpf(cpf)
     code = generate_verification_code()
-    await number_crud.update_verification_code(number, code)
+    await number_crud.update_verification_code(user.phone, code)
     message = f"Your recovery code is: {code}"
-    send_message(number, message)
+    send_message(user.phone, message)
+    return {"phone": user.phone}
 
 
-@router.get('/password_recovery/{number}/{code}', status_code=status.HTTP_200_OK)
-async def verify_recovery_code(number: str, code: int):
+@router.get('/password_recovery/{cpf}/{code}', status_code=status.HTTP_200_OK)
+async def verify_recovery_code(cpf: str, code: int):
     DEFAULT_PASSWORD = "Re1234@@"
-    number_model = await number_crud.get_number_model_by_number(number)
+    user = await user_crud.get_user_by_cpf(cpf)
+    number_model = await number_crud.get_number_model_by_number(user.phone)
     if number_model.verification_code:
         if number_model.verification_code == code:
             user = await user_crud.get_user_by_id(number_model.user_id)

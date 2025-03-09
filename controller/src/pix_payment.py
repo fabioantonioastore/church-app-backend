5da6c491c13ad365db2b52746f392f1e9917fa53
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2025-03-09 17:34:28 -0300

    bux fix

--- controller/src/pix_payment.py
+++ controller/src/pix_payment.py
@@ -77,7 +77,7 @@ def delete_pix_by_correlation_id(correlation_id: str) -> NoReturn:
 
 def is_pix_active(pix: dict, value: int | None) -> bool:
     if value:
-        return (pix["charge"]["status"] == ACTIVE) and (get_pix_value(pix) == value)
+        return (pix["charge"]["status"] == ACTIVE) and (get_pix_value(pix) == value / 100)
     return pix["charge"]["status"] == ACTIVE
 
 

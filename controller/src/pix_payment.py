bf2c79ea1febdad0fbddc44fb2a5447661f91707
ninja <astore.a.fabio@gmail.com>
2025-07-25 12:18:10 -0300

    fix

--- controller/src/pix_payment.py
+++ controller/src/pix_payment.py
@@ -50,7 +50,9 @@ def create_customer(user: User) -> dict:
 
 
 def make_post_pix_request(pix: PixPayment) -> dict:
+    print(pix)
     result = requests.post(url=PIX_COB_URL, headers=header, json=pix.__dict__())
+    print(result)
     return result.json()
 
 

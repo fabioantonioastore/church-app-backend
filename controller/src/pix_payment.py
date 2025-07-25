69ef5cca9c2249f085980f9a1e916ca384eac5e0
ninja <astore.a.fabio@gmail.com>
2025-07-25 13:39:53 -0300

    fix

--- controller/src/pix_payment.py
+++ controller/src/pix_payment.py
@@ -105,7 +105,7 @@ def delete_pix_by_correlation_id(correlation_id: str) -> NoReturn:
     return requests.delete(URL, headers=header)
 
 
-def is_pix_active(pix: dict, value: int) -> bool:
+def is_pix_active(pix: dict, value: int = None) -> bool:
     if value:
         return (pix["charge"]["status"] == ACTIVE) and (
             get_pix_value(pix) == (value / 100)
